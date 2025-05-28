d7e8e52218db79124258e6ac6d7b3ad6a9329bac
Churrosconmanjar <martin.a.e.oyarzun@gmail.com>
2025-05-28 19:56:19 -0400

    variable.h

---
+++ variable.h
@@ -0,0 +1,17 @@
+// Aqui se definen los pines para los 2 motores
+
+// Pines del Motor A (Motor 1)
+#define AIN1 16
+#define AIN2 17
+#define PWMA 4
+
+// Pines del Motor B (Motor 2)
+#define BIN1 5
+#define BIN2 18
+#define PWMB 19
+
+//Boton
+#define BOTON 12
+
+//Led
+#define LED 2
